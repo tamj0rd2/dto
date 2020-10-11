29ad3d7a0a167f107ace302d3c4f9f0bf9141ad8
Tamara Jordan <tamara.jordan1+coding@hotmail.com>
2020-10-11 12:42:42 +0100

    Do not allow generic functions

---
+++ src/__fixtures__/functions/generic.ts
@@ -0,0 +1,20 @@
+import { Dto } from '~dto'
+
+type MyFunc<R> = (name: string, age: number) => R
+
+export type GenericFunctionDto<R> = Dto<MyFunc<R>>
+
+// this should error because functions cannot be serialized
+export const dtoBad: GenericFunctionDto<[string, number]> = (name: string, age: number) => [name, age]
+
+// the rest of these should all error because we never want functions to go over
+// the wire at all. FunctionDto should be of type never
+export const dtoBadNull: GenericFunctionDto<[string, number]> = null
+
+export const dtoBadUndefined: GenericFunctionDto<[string, number]> = undefined
+
+export const dtoBadString: GenericFunctionDto<[string, number]> = '123897'
+
+export const dtoBadNumber: GenericFunctionDto<[string, number]> = 128376
+
+export const dtoBadObject: GenericFunctionDto<[string, number]> = { bad: 'times' }
