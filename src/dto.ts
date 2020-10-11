eaac9b878ce81ab7604f734eb0ccf97df10cc49f
Tamara Jordan <tamara.jordan1+coding@hotmail.com>
2020-10-11 17:07:26 +0100

    Refactor IsObject into IsValueType

--- src/dto.ts
+++ src/dto.ts
@@ -3,7 +3,7 @@
 
 type IsOptional<T> = Extract<T, undefined> extends never ? false : true
 type IsFunction<T> = T extends (...args: any[]) => any ? true : false
-type IsObject<T> = T extends
+type IsValueType<T> = T extends
   | string
   | number
   | boolean
@@ -13,22 +13,22 @@ type IsObject<T> = T extends
   | Set<any>
   | Map<any, any>
   | Date
-  ? false
-  : true
+  ? true
+  : false
 
 type ReplaceDate<T> = T extends Date ? string : T
 type ReplaceSet<T> = T extends Set<infer X> ? X[] : T
 type ReplaceMap<T> = T extends Map<infer K, infer I>
   ? K extends string | number | symbol
     ? Record<K, I>
-    : Record<string, I>
+    : T
   : T
 
 type ExcludeFuncsFromObj<T> = Pick<T, { [K in keyof T]: IsFunction<T[K]> extends true ? never : K }[keyof T]>
 
-type Dtoified<T> = IsObject<T> extends true
-  ? { [K in keyof ExcludeFuncsFromObj<T>]: Dto<T[K]> }
-  : ReplaceDate<ReplaceMap<ReplaceSet<T>>>
+type Dtoified<T> = IsValueType<T> extends true
+  ? ReplaceDate<ReplaceMap<ReplaceSet<T>>>
+  : { [K in keyof ExcludeFuncsFromObj<T>]: Dto<T[K]> }
 
 export type Dto<T> = IsFunction<T> extends true
   ? never
